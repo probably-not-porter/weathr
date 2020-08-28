b868b494f997a492f39e9b89a0df7953fa912d2d
probably-not-porter <pelibby16@earlham.edu>
2020-08-28 02:44:39 -0400

    organize layers, create functions

--- always.js
+++ always.js
@@ -1,10 +1,12 @@
-/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
+/*
+Weathr
+Pure CSS Weather display
+Porter L
+*/
+
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
-const msg = document.querySelector(".top-banner .msg");
-const list = document.querySelector(".ajax-section .cities");
-/*PUT YOUR OWN KEY HERE - THIS MIGHT NOT WORK
-SUBSCRIBE HERE: https://home.openweathermap.org/users/sign_up*/
+
 const apiKey = "8c2b3c56c116012fc50493ce48b8b0a5";
 
 form.addEventListener("submit", e => {
@@ -22,18 +24,130 @@ form.addEventListener("submit", e => {
         weather[0]["icon"]
       }@2x.png`;
 
-      if ((data.weather[0].icon.slice(-1)) == "n"){
-        document.getElementById("time-color").style.backgroundColor = "#222";
-      }
-      else if ((data.weather[0].icon.slice(-1)) == "d"){
-        document.getElementById("time-color").style.backgroundColor = "blue";
-      }
-      else{
-        console.log(data.weather[0].icon.slice(-1));
-      }
-    })
-
-  msg.textContent = "";
+      clear_layers() // clear all layers
+      select_layers(data.weather[0].icon.slice(0,2), data.weather[0].icon.slice(-1)) // selects appropriate layers
+  })
   form.reset();
   input.focus();
 });
+
+function select_layers(conditions, dayNight){
+  if (dayNight == "d"){ // currently day
+    if (conditions == "01"){ // clear sky
+      document.getElementById("color-bg-1").style.visibility = "visible";
+      document.getElementById("sun-bg").style.visibility = "visible";
+    }
+    else if (conditions == "02"){ // few clouds
+      document.getElementById("color-bg-2").style.visibility = "visible";
+      document.getElementById("sun-bg").style.visibility = "visible";
+      document.getElementById("clouds1-bg").style.visibility = "visible";
+    }
+    else if (conditions == "03"){ // scattered clouds
+      document.getElementById("color-bg-2").style.visibility = "visible";
+      document.getElementById("sun-bg").style.visibility = "visible";
+      document.getElementById("clouds1-bg").style.visibility = "visible";
+
+    }
+    else if (conditions == "04"){ // broken clouds
+      document.getElementById("color-bg-2").style.visibility = "visible";
+      document.getElementById("sun-bg").style.visibility = "visible";
+      document.getElementById("clouds2-bg").style.visibility = "visible";
+
+    }
+    else if (conditions == "09"){ // shower rain
+      document.getElementById("color-bg-3").style.visibility = "visible";
+      document.getElementById("clouds2-bg").style.visibility = "visible";
+      document.getElementById("rain1-bg").style.visibility = "visible";
+
+    }
+    else if (conditions == "10"){ // rain
+      document.getElementById("color-bg-3").style.visibility = "visible";
+      document.getElementById("rain2-bg").style.visibility = "visible";
+
+    }
+    else if (conditions == "11"){ // thunderstorm
+      document.getElementById("color-bg-3").style.visibility = "visible";
+      document.getElementById("rain2-bg").style.visibility = "visible";
+
+    }
+    else if (conditions == "13"){ // snow
+      document.getElementById("color-bg-2").style.visibility = "visible";
+
+    }
+    else if (conditions == "50"){ // mist
+      document.getElementById("color-bg-2").style.visibility = "visible";
+
+    }
+    else{
+      console.error("Invalid weather state");
+    }
+  }
+  else if(dayNight == "n"){ // currently night
+    if (conditions == "01"){ // clear sky
+      document.getElementById("color-bg-4").style.visibility = "visible";
+      document.getElementById("moon-bg").style.visibility = "visible";
+
+    }
+    else if (conditions == "02"){ // few clouds
+      document.getElementById("color-bg-4").style.visibility = "visible";
+      document.getElementById("moon-bg").style.visibility = "visible";
+      document.getElementById("clouds1-bg").style.visibility = "visible";
+
+    }
+    else if (conditions == "03"){ // scattered clouds
+      document.getElementById("color-bg-4").style.visibility = "visible";
+      document.getElementById("moon-bg").style.visibility = "visible";
+      document.getElementById("clouds1-bg").style.visibility = "visible";
+
+    }
+    else if (conditions == "04"){ // broken clouds
+      document.getElementById("color-bg-4").style.visibility = "visible";
+      document.getElementById("moon-bg").style.visibility = "visible";
+      document.getElementById("clouds2-bg").style.visibility = "visible";
+
+    }
+    else if (conditions == "09"){ // shower rain
+      document.getElementById("color-bg-4").style.visibility = "visible";
+      document.getElementById("clouds2-bg").style.visibility = "visible";
+      document.getElementById("rain1-bg").style.visibility = "visible";
+
+    }
+    else if (conditions == "10"){ // rain
+      document.getElementById("color-bg-4").style.visibility = "visible";
+      document.getElementById("rain2-bg").style.visibility = "visible";
+
+    }
+    else if (conditions == "11"){ // thunderstorm
+      document.getElementById("color-bg-4").style.visibility = "visible";
+      document.getElementById("rain2-bg").style.visibility = "visible";
+
+    }
+    else if (conditions == "13"){ // snow
+      document.getElementById("color-bg-4").style.visibility = "visible";
+      document.getElementById("moon-bg").style.visibility = "visible";
+
+    }
+    else if (conditions == "50"){ // mist
+      document.getElementById("color-bg-4").style.visibility = "visible";
+      document.getElementById("moon-bg").style.visibility = "visible";
+
+    }
+    else{
+      console.error("Invalid weather state");
+    }
+  }
+  else{
+    console.error("Invalid Day-Night state");
+  }
+}
+
+function clear_layers(){
+  var elems = document.getElementsByClassName('weather-layer'),
+  size = elems.length;
+  for (var i = 0; i < size; i++) {
+    var layer = elems[i];
+    layer.style.visibility = "hidden"; // set layer to hidden
+  }
+}
+
+clear_layers() // clear all layers
